a174ed7c04615af70be1133370dac5ed2639e939
Benji-UW <bpa13@uw.edu>
2021-08-02 15:22:54 -0700

    My pirated version should work now

--- BenPiratingPicoscopeScripts.m
+++ BenPiratingPicoscopeScripts.m
@@ -192,7 +192,7 @@ triggerGroupObj = triggerGroupObj(1);
 set(triggerGroupObj, 'autoTriggerMs', 0);
 
 % Channel     : 0 (ps5000aEnuminfo.enPS5000AChannel.PS5000A_CHANNEL_A)
-% Threshold   : 500 mV
+% Threshold   : 100 mV (edited)
 % Direction   : 2 (ps5000aEnuminfo.enPS5000AThresholdDirection.PS5000A_RISING)
 
 [status.setSimpleTrigger] = invoke(triggerGroupObj, 'setSimpleTrigger', 0, 100, 2);
@@ -235,6 +235,360 @@ status.setAppDriverBuffersA = invoke(streamingGroupObj, 'setAppAndDriverBuffers'
 % status.setAppDriverBuffersB = invoke(streamingGroupObj, 'setAppAndDriverBuffers', channelB, ...
 %     pAppBufferChB, pDriverBufferChB, overviewBufferSize);
 
+%% Start streaming and collect data
+% Use default value for streaming interval which is 1e-6 for 1 MS/s.
+% Collect data for 5 seconds with auto stop - maximum array size will depend
+% on the PC's resources - type <matlab:doc('memory') |memory|> at the
+% MATLAB command prompt for further information.
+%
+% To change the sample interval set the |streamingInterval| property of the
+% Streaming group object. The call to the |ps5000aRunStreaming()| function
+% will output the actual sampling interval used by the driver.
+
+% To change the sample interval e.g 5 us for 200 kS/s
+% set(streamingGroupObj, 'streamingInterval', 5e-6);
+
+%%
+% Set the number of pre- and post-trigger samples.
+% If no trigger is set the library will still store
+% |numPreTriggerSamples| + |numPostTriggerSamples|.
+set(ps5000aDeviceObj, 'numPreTriggerSamples', 1000);
+% I'm setting the number of post-trigger samples to be 10 kS, because by default
+% the streaming interval is 1 MS/s and the frequency of the pulser/receiver is 
+% 100 Hz, meaning we want weach sample to capture just one of those
+set(ps5000aDeviceObj, 'numPostTriggerSamples', 9000); % 1e4
+
+%%
+% The |autoStop| parameter can be set to false (0) to allow for continuous
+% data collection.
+set(streamingGroupObj, 'autoStop', PicoConstants.FALSE);
+
+% Set other streaming parameters
+downSampleRatio = 1;
+% downSampleRatioMode = ps5000aEnuminfo.enPS5000ARatioMode.PS5000A_RATIO_MODE_NONE;
+
+%%
+% Defined buffers to store data collected from the channels. If capturing
+% data without using the autoStop flag, or if using a trigger with the
+% autoStop flag, allocate sufficient space (1.5 times the sum of the number
+% of pre-trigger and post-trigger samples is shown below) to allow for
+% additional pre-trigger data. Pre-allocating the array is more efficient
+% than using <matlab:doc('vertcat') |vertcat|> to combine data.
+
+maxSamples = get(ps5000aDeviceObj, 'numPreTriggerSamples') + ...
+    get(ps5000aDeviceObj, 'numPostTriggerSamples');
+
+% Take into account the downsampling ratio mode - required if collecting
+% data without a trigger and using the autoStop flag.
+
+finalBufferLength = round(1.5 * maxSamples / downSampleRatio);
+
+pBufferChAFinal = libpointer('int16Ptr', zeros(finalBufferLength, 1, 'int16'));
+% pBufferChBFinal = libpointer('int16Ptr', zeros(finalBufferLength, 1, 'int16'));
+
+% Prompt User to indicate if they wish to plot live streaming data.
+plotLiveData = questionDialog('Plot live streaming data?', 'Streaming Data Plot');
+
+if (plotLiveData == PicoConstants.TRUE)
+   
+    disp('Live streaming data collection with second plot on completion.');
+    
+else
+    
+    disp('Streaming data plot on completion.');
+    
+end
+
+originalPowerSource = invoke(ps5000aDeviceObj, 'ps5000aCurrentPowerSource');
+
+% Start streaming data collection.
+[status.runStreaming, sampleInterval, sampleIntervalTimeUnitsStr] = ...
+    invoke(streamingGroupObj, 'ps5000aRunStreaming', downSampleRatio, ...
+    downSampleRatioMode, overviewBufferSize);
+    
+disp('Streaming data...');
+fprintf('Click the STOP button to stop capture or wait for auto stop if enabled.\n') 
+
+% Variables to be used when collecting the data:
+
+hasAutoStopOccurred = PicoConstants.FALSE;  % Indicates if the device has stopped automatically.
+powerChange         = PicoConstants.FALSE;  % If the device power status has changed.
+newSamples          = 0; % Number of new samples returned from the driver.
+previousTotal       = 0; % The previous total number of samples.
+totalSamples        = 0; % Total samples captured by the device.
+startIndex          = 0; % Start index of data in the buffer returned.
+hasTriggered        = 0; % To indicate if trigger has occurred.
+triggeredAtIndex    = 0; % The index in the overall buffer where the trigger occurred.
+
+time = zeros(overviewBufferSize / downSampleRatio, 1);	% Array to hold time values
+
+status.getStreamingLatestValuesStatus = PicoStatus.PICO_OK; % OK
+
+% Display a 'Stop' button.
+[stopFig.h, stopFig.h] = stopButton();             
+             
+flag = 1; % Use flag variable to indicate if stop button has been clicked (0).
+setappdata(gcf, 'run', flag);
+
+% Plot Properties - these are for displaying data as it is collected.
+
+if (plotLiveData == PicoConstants.TRUE)
+    
+    % Plot on a single figure. 
+    figure1 = figure('Name','PicoScope 5000 Series (A API) Example - Streaming Mode Capture', ...
+         'NumberTitle','off');
+     
+    axes1 = axes('Parent', figure1);
+
+    % Estimate x-axis limit to try and avoid using too much CPU resources
+    % when drawing - use max voltage range selected if plotting multiple
+    % channels on the same graph.
+    xlim(axes1, [0 (sampleInterval * finalBufferLength)]);
+
+    yRange = max(channelARangeMv, channelBRangeMv);
+    ylim(axes1,[(-1 * yRange) yRange]);
+
+    hold(axes1,'on');
+    grid(axes1, 'on');
+
+    title(axes1, 'Live Streaming Data Capture');
+    
+    if (strcmp(sampleIntervalTimeUnitsStr, 'us'))
+        
+        xLabelStr = 'Time (\mus)';
+        
+    else
+       
+        xLabelStr = strcat('Time (', sampleIntervalTimeUnitsStr, ')');
+        xlabel(axes1, xLabelStr);
+        
+    end
+    
+    xlabel(axes1, xLabelStr);
+    ylabel(axes1, 'Voltage (mV)');
+    
+end
+
+%%
+% Collect samples as long as the |hasAutoStopOccurred| flag has not been
+% set or the call to |getStreamingLatestValues()| does not return an error
+% code (check for STOP button push inside loop).
+while(hasAutoStopOccurred == PicoConstants.FALSE && status.getStreamingLatestValuesStatus == PicoStatus.PICO_OK)
+    
+    ready = PicoConstants.FALSE;
+   
+    while (ready == PicoConstants.FALSE)
+
+       status.getStreamingLatestValuesStatus = invoke(streamingGroupObj, 'getStreamingLatestValues'); 
+        
+       ready = invoke(streamingGroupObj, 'isReady');
+
+       % Give option to abort from here
+       flag = getappdata(gcf, 'run');
+       drawnow;
+
+       if (flag == 0)
+
+            disp('STOP button clicked - aborting data collection.')
+            break;
+
+       end
+
+       drawnow;
+
+    end
+    
+    % Check for data
+    [newSamples, startIndex] = invoke(streamingGroupObj, 'availableData');
+
+    if (newSamples > 0)
+        
+        % Check if the scope has triggered.
+        [triggered, triggeredAt] = invoke(streamingGroupObj, 'isTriggerReady');
+
+        if (triggered == PicoConstants.TRUE)
+
+            % Adjust trigger position as MATLAB does not use zero-based
+            % indexing.
+            bufferTriggerPosition = triggeredAt + 1;
+            
+            fprintf('Triggered - index in buffer: %d\n', bufferTriggerPosition);
+
+            hasTriggered = triggered;
+
+            % Set the total number of samples at which the device
+            % triggered.
+            triggeredAtIndex = totalSamples + bufferTriggerPosition;
+
+        end
+
+        previousTotal   = totalSamples;
+        totalSamples    = totalSamples + newSamples;
+
+        % Printing to console can slow down acquisition - use for
+        % demonstration.
+        fprintf('Collected %d samples, startIndex: %d total: %d.\n', newSamples, startIndex, totalSamples);
+        
+        % Position indices of data in the buffer(s).
+        firstValuePosn = startIndex + 1;
+        lastValuePosn = startIndex + newSamples;
+        
+        % Convert data values to millivolts from the application buffer(s).
+        bufferChAmV = adc2mv(pAppBufferChA.Value(firstValuePosn:lastValuePosn), channelARangeMv, maxADCCount);
+%         bufferChBmV = adc2mv(pAppBufferChB.Value(firstValuePosn:lastValuePosn), channelBRangeMv, maxADCCount);
+
+        % Process collected data further if required - this example plots
+        % the data if the User has selected 'Yes' at the prompt.
+        
+        % Copy data into the final buffer(s).
+        pBufferChAFinal.Value(previousTotal + 1:totalSamples) = bufferChAmV;
+%         pBufferChBFinal.Value(previousTotal + 1:totalSamples) = bufferChBmV;
+        
+        if (plotLiveData == PicoConstants.TRUE)
+            
+            % Time axis. 
+            % Multiply by ratio mode as samples get reduced
+            time = (double(sampleInterval) * double(downSampleRatio)) * (previousTotal:(totalSamples - 1));
+
+            plot(axes1, time, bufferChAmV); %, time, bufferChBmV);
+        
+        end
+       
+        % Clear variables for use again
+        clear bufferChAmV;
+%         clear bufferChBmV;
+        clear firstValuePosn;
+        clear lastValuePosn;
+        clear startIndex;
+        clear triggered;
+        clear triggerAt;
+          
+   end
+   
+    % Check if auto stop has occurred.
+    hasAutoStopOccurred = invoke(streamingGroupObj, 'autoStopped');
+
+    if (hasAutoStopOccurred == PicoConstants.TRUE)
+
+       disp('AutoStop: TRUE - exiting data collection loop.');
+       break;
+
+    end
+   
+    % Check if 'STOP' button has been clicked.
+    flag = getappdata(gcf, 'run');
+    drawnow;
+
+    if (flag == 0)
+
+        disp('STOP button clicked - aborting data collection.')
+        break;
+        
+    end
+ 
+end
+
+% Close the STOP button window.
+if (exist('stopFig', 'var'))
+    
+    close('Stop Button');
+    clear stopFig;
+        
+end
+
+if (plotLiveData == PicoConstants.TRUE)
+    
+    drawnow;
+    
+    % Take hold off the current figure.
+    hold(axes1, 'off');
+    movegui(figure1, 'west');
+    
+end
+
+if (hasTriggered == PicoConstants.TRUE)
+   
+    fprintf('Triggered at overall index: %d\n\n', triggeredAtIndex);
+    
+end
+
+fprintf('\n');
+
+%% Stop the device
+% This function should be called regardless of whether the autoStop
+% property is enabled or not.
+
+[status.stop] = invoke(ps5000aDeviceObj, 'ps5000aStop');
+
+%% Find the number of samples
+% This is the number of samples held in the shared library itself. The
+% actual number of samples collected when using a trigger is likely to be
+% greater.
+
+[status.noOfStreamingValues, numStreamingValues] = invoke(streamingGroupObj, 'ps5000aNoOfStreamingValues');
+
+fprintf('Number of samples available after data collection: %u\n', numStreamingValues);
+
+%% Process data
+% Process data post-capture if required - here the data will be plotted.
+
+% Reduce size of arrays if required.
+
+if (totalSamples < finalBufferLength)
+    
+    pBufferChAFinal.Value(totalSamples + 1:end) = [];
+%     pBufferChBFinal.Value(totalSamples + 1:end) = [];
+
+end
+
+% Retrieve data for the channels.
+channelAFinal = pBufferChAFinal.Value();
+% channelBFinal = pBufferChBFinal.Value();
+
+% Plot total data collected on another figure.
+
+finalFigure = figure('Name','PicoScope 5000 Series (A API) Example - Streaming Mode Capture', ...
+    'NumberTitle','off');
+
+finalFigureAxes = axes('Parent', finalFigure);
+hold(finalFigureAxes, 'on');
+grid(finalFigureAxes, 'on');
+
+if (strcmp(sampleIntervalTimeUnitsStr, 'us'))
+        
+    xlabel(finalFigureAxes, 'Time (\mus)');
+
+else
+
+    xLabelStr = strcat('Time (', sampleIntervalTimeUnitsStr, ')');
+    xlabel(finalFigureAxes, xLabelStr);
+
+end
+
+ylabel(finalFigureAxes, 'Voltage (mV)');
+hold(finalFigureAxes, 'off');
+
+time = (double(sampleInterval) * double(downSampleRatio)) * (0:length(channelAFinal) - 1);
+
+% Channel A
+chAAxes = subplot(2,1,1); 
+plot(chAAxes, time, channelAFinal, 'b');
+xLabelStr = strcat('Time (', sampleIntervalTimeUnitsStr, ')');
+xlabel(chAAxes, xLabelStr);
+ylabel(chAAxes, 'Voltage (mV)');
+title(chAAxes, 'Data acquisition on channel A (Final)');
+grid(chAAxes, 'on');
+
+% % Channel B
+% chBAxes = subplot(2,1,2); 
+% plot(chBAxes, time, channelBFinal, 'r');
+% title(chBAxes, 'Data acquisition on channel B (Final)');
+% xLabelStr = strcat('Time (', sampleIntervalTimeUnitsStr, ')');
+% xlabel(chBAxes, xLabelStr);
+% ylabel(chBAxes, 'Voltage (mV)');
+% grid(chBAxes, 'on');
+
+movegui(finalFigure, 'east');
 
 
 
