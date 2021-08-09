c661d870d8160e17ea68b4bcb043ecd066cfe31c
Benji-UW <bpa13@uw.edu>
2021-08-09 11:15:25 -0700

    Completed copying a lot more control code into the script, next step will be using this to actually control the robot, this is gonna require some signal processing :/

--- Picoscope_z_depth_detect.m
+++ Picoscope_z_depth_detect.m
@@ -89,14 +89,217 @@ TriggerDirections(1).mode = ps5000aEnuminfo.enPS5000AThresholdMode.PS5000A_LEVEL
 [status.setTriggerChannelDirectionsV2] = invoke(triggerGroupObj, ...
     'ps5000aSetTriggerChannelDirectionsV2', TriggerDirections);
 
+%% Resolution settings
 
-overviewBufferSize  = 250000; % Size of the buffer to collect data from buffer.
+sampleRate = 2e-8; % Sample period, this helps us calculate several other values
+% We only really want the 90 us after the trigger, so a lot of data is just
+% going down the tubes as far as we're concerned.
+samplesPerSecond = 1 / sampleRate;
+samplePeriod = 1e-4; % 100 us
+
+% Size of the buffer to collect data from buffer.
+overviewBufferSize  = samplesPerSecond  * samplePeriod;
 segmentIndex        = 0;   
 ratioMode           = ps5000aEnuminfo.enPS5000ARatioMode.PS5000A_RATIO_MODE_NONE;
 
+pDriverBufferChA = libpointer('int16Ptr', zeros(overviewBufferSize, 1, ...
+    'int16'));
+
+status.setDataBufferChA = invoke(ps5000aDeviceObj, 'ps5000aSetDataBuffer', ...
+    channelA, pDriverBufferChA, overviewBufferSize, segmentIndex, ratioMode);
+
+pAppBufferChA = libpointer('int16Ptr', zeros(overviewBufferSize, 1, 'int16'));
+
+streamingGroupObj = get(ps5000aDeviceObj, 'Streaming');
+streamingGroupObj = streamingGroupObj(1);
+
+status.setAppDriverBuffersA = invoke(streamingGroupObj, 'setAppAndDriverBuffers', channelA, ...
+    pAppBufferChA, pDriverBufferChA, overviewBufferSize);
+
+% Sample rate is defined by the 
+set(streamingGroupObj, 'streamingInterval', sampleRate);
+
+set(ps5000aDeviceObj, 'numPreTriggerSamples', ...
+    (samplesPerSecond * SamplePeriod) * 0.1);
+set(ps5000aDeviceObj, 'numPostTriggerSamples', ...
+    (samplesPerSecond * SamplePeriod) * 0.9); % 1e4
 
 
+set(streamingGroupObj, 'autoStop', PicoConstants.FALSE);
+
+downSampleRatio = 1;
+downSampleRatioMode = ps5000aEnuminfo.enPS5000ARatioMode.PS5000A_RATIO_MODE_NONE;
+ 
+maxSamples = get(ps5000aDeviceObj, 'numPreTriggerSamples') + ...
+    get(ps5000aDeviceObj, 'numPostTriggerSamples');
+
+finalBufferLength = round(1.5 * maxSamples / downSampleRatio);
+
+pBufferChAFinal = libpointer('int16Ptr', zeros(finalBufferLength, ...
+    1, 'int16'));
 
 
+originalPowerSource = invoke(ps5000aDeviceObj, 'ps5000aCurrentPowerSource');
+
+% Start streaming data collection.
+[status.runStreaming, sampleInterval, sampleIntervalTimeUnitsStr] = ...
+    invoke(streamingGroupObj, 'ps5000aRunStreaming', downSampleRatio, ...
+    downSampleRatioMode, overviewBufferSize);
+
+disp('Streaming data...');
+fprintf('Click the STOP button to stop capture or wait for auto stop if enabled.\n') 
+
+% Variables to be used when collecting the data:
+
+hasAutoStopOccurred = PicoConstants.FALSE;  % Indicates if the device has stopped automatically.
+powerChange         = PicoConstants.FALSE;  % If the device power status has changed.
+newSamples          = 0; % Number of new samples returned from the driver.
+previousTotal       = 0; % The previous total number of samples.
+totalSamples        = 0; % Total samples captured by the device.
+startIndex          = 0; % Start index of data in the buffer returned.
+hasTriggered        = 0; % To indicate if trigger has occurred.
+triggeredAtIndex    = 0; % The index in the overall buffer where the trigger occurred.
+
+time = zeros(overviewBufferSize / downSampleRatio, 1);	% Array to hold time values
+
+status.getStreamingLatestValuesStatus = PicoStatus.PICO_OK; % OK
+
+% Display a 'Stop' button.
+[stopFig.h, stopFig.h] = stopButton();             
+             
+flag = 1; % Use flag variable to indicate if stop button has been clicked (0).
+setappdata(gcf, 'run', flag);
+
+% Plot Properties - these are for displaying data as it is collected.
 
 
+%%
+% Collect samples as long as the |hasAutoStopOccurred| flag has not been
+% set or the call to |getStreamingLatestValues()| does not return an error
+% code (check for STOP button push inside loop).
+while(hasAutoStopOccurred == PicoConstants.FALSE && status.getStreamingLatestValuesStatus == PicoStatus.PICO_OK)
+    tic
+    ready = PicoConstants.FALSE;
+    
+    while (ready == PicoConstants.FALSE)
+
+       status.getStreamingLatestValuesStatus = invoke(streamingGroupObj, 'getStreamingLatestValues'); 
+        
+       ready = invoke(streamingGroupObj, 'isReady');
+
+       % Give option to abort from here
+       flag = getappdata(gcf, 'run');
+       drawnow;
+
+       if (flag == 0)
+
+            disp('STOP button clicked - aborting data collection.')
+            break;
+
+       end
+
+       drawnow;
+
+    end
+    
+    % Check for data
+    [newSamples, startIndex] = invoke(streamingGroupObj, 'availableData');
+
+    if (newSamples > 0)
+        
+        % Check if the scope has triggered.
+        [triggered, triggeredAt] = invoke(streamingGroupObj, 'isTriggerReady');
+
+        if (triggered == PicoConstants.TRUE)
+
+            % Adjust trigger position as MATLAB does not use zero-based
+            % indexing.
+            bufferTriggerPosition = triggeredAt + 1;
+            
+            fprintf('Triggered - index in buffer: %d\n', bufferTriggerPosition);
+
+            hasTriggered = triggered;
+
+            % Set the total number of samples at which the device
+            % triggered.
+            triggeredAtIndex = totalSamples + bufferTriggerPosition;
+
+        end
+
+        previousTotal   = totalSamples;
+        totalSamples    = totalSamples + newSamples;
+
+        % Printing to console can slow down acquisition - use for
+        % demonstration.
+        fprintf('Collected %d samples, startIndex: %d total: %d.\n', newSamples, startIndex, totalSamples);
+        
+        % Position indices of data in the buffer(s).
+        firstValuePosn = startIndex + 1;
+        lastValuePosn = startIndex + newSamples;
+        
+        % Convert data values to millivolts from the application buffer(s).
+        bufferChAmV = adc2mv(pAppBufferChA.Value(firstValuePosn:lastValuePosn), channelARangeMv, maxADCCount);
+
+        % Process collected data further if required - this example plots
+        % the data if the User has selected 'Yes' at the prompt.
+        
+        % Copy data into the final buffer(s).
+        pBufferChAFinal.Value(previousTotal + 1:totalSamples) = bufferChAmV;
+        toc
+      
+        % Clear variables for use again
+        clear bufferChAmV;
+        clear firstValuePosn;
+        clear lastValuePosn;
+        clear startIndex;
+        clear triggered;
+        clear triggerAt;
+        
+   end
+   
+    % Check if auto stop has occurred.
+    hasAutoStopOccurred = invoke(streamingGroupObj, 'autoStopped');
+
+    if (hasAutoStopOccurred == PicoConstants.TRUE)
+
+       disp('AutoStop: TRUE - exiting data collection loop.');
+       break;
+
+    end
+   
+    % Check if 'STOP' button has been clicked.
+    flag = getappdata(gcf, 'run');
+    drawnow;
+
+    if (flag == 0)
+
+        disp('STOP button clicked - aborting data collection.')
+        break;
+        
+    end
+ 
+end
+
+% Close the STOP button window.
+if (exist('stopFig', 'var'))
+    
+    close('Stop Button');
+    clear stopFig;
+        
+end
+
+if (hasTriggered == PicoConstants.TRUE)
+   
+    fprintf('Triggered at overall index: %d\n\n', triggeredAtIndex);
+    
+end
+
+fprintf('\n');
+
+
+%% Disconnect device
+% Disconnect device object from hardware.
+[status.stop] = invoke(ps5000aDeviceObj, 'ps5000aStop');
+
+disconnect(ps5000aDeviceObj);
+delete(ps5000aDeviceObj);
