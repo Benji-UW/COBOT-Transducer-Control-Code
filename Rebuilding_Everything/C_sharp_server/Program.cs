d10243ef710eb7ae3b6385785db619ae8acf703b
Benji-UW <bpa13@uw.edu>
2022-06-30 12:19:39 -0700

    Working on making a server in C#, cuz it's taking too long running on the python one. Also maybe I can increase the strength of my message parsing.

---
+++ Rebuilding_Everything/C_sharp_server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello               there!");
+            Console.WriteLine("General Kenobi!");
+            Console.ReadLine();
+            // return 1;
+        }
+    }
+}
