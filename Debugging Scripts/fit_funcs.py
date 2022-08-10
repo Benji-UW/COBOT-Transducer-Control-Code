b07d31004e2a35de422307127b74838de7d2079c
Benji-UW <bpa13@uw.edu>
2022-08-09 18:10:40 -0700

    Mostly just presentation prep, plus I added some initial guesses to the 2d curve_fits

--- Debugging Scripts/fit_funcs.py
+++ Debugging Scripts/fit_funcs.py
@@ -9,78 +9,63 @@ def r_squared(func, popt, x_dat, y_dat):
     return 1 - (ss_res/ss_tot)
 
 def polynomial_2(x, a, b, c):
-    '''2nd order polynomial (is terrible)'''
+    '''2nd order polynomial'''
     return (a*(x**2)) + (b*x) + c
 
 def rational_2(x,a,b,c,d):
     '''2nd Order rational function'''
     return (1/((a*(x**2)) + (b*x) + c))
 
-def gauss(x,a,b,c):
-    '''Gaussian'''
-    return (a * (np.e)**(-b * (x-c)**2))
+def gauss(x,a,b,x_offset,y_offset):
+    '''Gaussian fit'''
+    return (a * (np.e)**(-b * (x-x_offset)**2)) + y_offset
 
-def gauss_plus(x,a,b,c,d):
-    '''Gaussian plus'''
-    return (a * (np.e)**(-b * (x-c)**2)) + d
-
-def arctan(x,a,b,c):
-    '''b * arctan(c/(x-a)^2)'''
-    return (b * np.arctan(c/(x-a)**2))
+def arctan(x,a,b,x_offset):
+    '''a * arctan(b/(x-x_offset)^2)'''
+    return (a * np.arctan(b/(x-x_offset)**2))
 
 def rational_3(x,a,b,c,d):
     '''Third order rational function 8/'''
     return ((1/((a*(x**3)) + (b*(x**2)) + c*x + d)))
 
-def absolute(x,a,b,c):
+def absolute(x,a,b,x_offset):
     '''Simple absolute value function'''
-    return a * np.abs((x-b)) + c
+    return a * np.abs((x-x_offset)) + b
 
-def inv_absolute(x,a,b,c):
+def inv_absolute(x,a,b,x_offset,y_offset):
     '''Rational absolute value function'''
-    return (a / (np.abs((x-b)) + c))
+    return (a / (np.abs((x-x_offset)) + b)) + y_offset
 
-def inv_abs_poly(x,a,b,c):
+def inv_abs_poly(x,a,b,x_offset):
     '''Rational absolute value polynomial'''
-    return a / (np.abs((x-b)**2 + c))
+    return a / (np.abs((x-x_offset)**2 + b))
 
-def inv_abs_poly_4(x,a,b,c,d,e,f,g,h):
+def inv_abs_poly_4(x,a,b,c,d,e,f,y_offset):
     '''Rational absolute value 4th degree polynomial'''
-    return (a / (np.abs(x**4 + (x*c)**3 + (x*d)**2 + (x*e) + f) + g)) + h
+    return (a / (np.abs(x**4 + (x*b)**3 + (x*c)**2 + (x*d) + e) + f)) + y_offset
 
 
 
 
 
-def ang_plus_z(data,a,b,c,d,e,f,g):
+def ang_plus_z(data,a,b,c,d,x_offset,y_offset,z_offset):
     '''The inv_absolute should be applied to the Z data, gauss to the Ry data'''
     y=data[0]
     x=data[1]
 
-    return (gauss(x,a,b,c) * inv_absolute(y,d,e,f)) + g
+    return (gauss(x,a,b,x_offset,z_offset) * inv_absolute(y,c,d,y_offset,0))
 
-def double_ang(data,a,b,c):
-    # print(data)
+def double_ang(data,a,b,x_offset,y_offset,z_offset):
     x=data[0]
     y=data[1]
-    # print(x[0:10])
-    # print(y[0:10])
-    # print(np.deg2rad(x)[0:10])
-    # print(np.deg2rad(y)[0:10])
-    # print(np.cos(np.deg2rad(x)[0:10]))
-    # print(np.cos(np.deg2rad(y)[0:10]))
-    # t = np.rad2deg(np.arccos(np.cos(np.deg2rad(y)) * np.cos(np.deg2rad(x))))
-    
-    # print('------------------')
-    # print(t[0:10])
 
+    return (a * (np.e)**(-b * (np.arccos(np.cos(np.deg2rad(x-x_offset)) * np.cos(np.deg2rad(y-y_offset))))**2)) + z_offset
+    # return (a * (np.e)**(-b * (np.arccos(np.cos(np.deg2rad(x-x_offset)) * np.cos(np.deg2rad(y))))**2)) + z_offset
 
-    return (a * (np.e)**(-b * (np.arccos(np.cos(np.deg2rad(x)) * np.cos(np.deg2rad(y))))**2)) + c
-
-def Z_Rx_Ry(data,a,b,c,d,e,f,g):
+def Z_Rx_Ry(data,a,b,c,d,Rx_offset,Ry_offset,z_offset,mag_offset):
     '''Must be passed the data in the form [Z,Rx,Ry]'''
     z=data[0]
     Rx=data[1]
     Ry=data[2]
 
-    return (double_ang([Rx,Ry], a,b,c) * inv_absolute(z,d,e,f)) + g
+    return (double_ang([Rx,Ry],a,b,Rx_offset,Ry_offset,mag_offset) * inv_absolute(z,c,d,z_offset,0)) + mag_offset
